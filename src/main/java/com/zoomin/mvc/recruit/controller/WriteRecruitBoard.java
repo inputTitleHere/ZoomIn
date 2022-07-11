a6d51bb0766f8d80801e9ab6588bdb41258e084d
inputTitleHere <seanbryan97@naver.com>
2022-07-11 21:39:43 +0900

    Serlvet Change

--- src/main/java/com/zoomin/mvc/recruit/controller/WriteRecruitBoard.java
+++ src/main/java/com/zoomin/mvc/recruit/controller/WriteRecruitBoard.java
@@ -22,12 +22,6 @@ public class WriteRecruitBoard extends HttpServlet {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	
 
 }
