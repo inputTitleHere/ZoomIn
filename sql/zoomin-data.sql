3be330e70b12dc03694faf0500663535d6a57cf3
echojykim <honbaegsan@gmail.com>
2022-07-13 16:45:08 +0900

    db 문장 하나 추가했습니다 -지윤

--- sql/zoomin-data.sql
+++ sql/zoomin-data.sql
@@ -12,7 +12,7 @@
 
 -- 김승환 END --
 -- 김지윤 START -- 
-
+insert into APPLICANT_MEMBER values(seq_applicant_member_uid.nextval, '김지윤', 'yuiop', '1234', '01028013333', 'google@gmail.com', default);
 
 -- 김지윤 END -- 
 -- 이윤정 START --
