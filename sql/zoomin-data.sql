15d7d2fba887a03269de031746502378718ce127
inputTitleHere <seanbryan97@naver.com>
2022-07-13 16:46:46 +0900

    zoomin-data.sql에 구직자 멤버데이터 추가

--- sql/zoomin-data.sql
+++ sql/zoomin-data.sql
@@ -1,8 +1,9 @@
 -- 백승윤 START --
 select * from applicant_member;
 insert into APPLICANT_MEMBER values(seq_applicant_member_uid.nextval, '백승윤', 'zxcv', 1234, 01035351212, 'emailsemail@mail.com', default);
+insert into APPLICANT_MEMBER values(seq_applicant_member_uid.nextval, '백길동', 'baikgd',1234,01099321182, 'baikgildong@mail.com',default);
 
-
+commit;
 -- 백승윤 END
 -- 박우석 START-- 
 
