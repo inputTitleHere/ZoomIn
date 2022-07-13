cba57630905eb6f77d744736840f481bed160d3f
wooooseok <pws6882586@naver.com>
2022-07-13 16:45:12 +0900

    내이름 확인

---
+++ sql/zoomin-data.sql
@@ -0,0 +1,22 @@
+-- 백승윤 START --
+
+
+-- 백승윤 END
+-- 박우석 START-- 
+select * from applicant_member;
+insert into applicant_member values (seq_applicant_member_uid.nextval, '박우석', 'abcd', 1111, 01012345678, 'abcd@abcd.com', default);
+
+-- 박우석 END -- 
+-- 김승환 START -- 
+
+
+-- 김승환 END --
+-- 김지윤 START -- 
+
+
+-- 김지윤 END -- 
+-- 이윤정 START --
+
+
+-- 이윤정 END --
+
