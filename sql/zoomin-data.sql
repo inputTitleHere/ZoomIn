0d01dfe2338a78f18dc72e99a5c3c7e5ac6f35b6
echojykim <honbaegsan@gmail.com>
2022-07-13 16:50:49 +0900

    db 추가 작성.

--- sql/zoomin-data.sql
+++ sql/zoomin-data.sql
@@ -14,7 +14,8 @@ insert into APPLICANT_MEMBER values(seq_applicant_member_uid.nextval, '백승윤
 -- 김승환 END --
 -- 김지윤 START -- 
 insert into APPLICANT_MEMBER values(seq_applicant_member_uid.nextval, '김지윤', 'yuiop', '1234', '01028013333', 'google@gmail.com', default);
-
+insert into APPLICANT_MEMBER values(seq_applicant_member_uid.nextval, '김수연', 'suyyen', '1234', '01054548989', 'suyyen@naver.com', default);
+insert into APPLICANT_MEMBER values(seq_applicant_member_uid.nextval, '하민정', 'heymin', '1234', '01022218005', 'heymin@naver.com', default);
 -- 김지윤 END -- 
 -- 이윤정 START --
 
